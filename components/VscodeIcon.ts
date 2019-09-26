b9f24310664bdb192287b0720284485f763e3991
bendera <szoftverhiba@gmail.com>
2019-09-26 17:45:46 +0200

    rewrite icon component

--- components/VscodeIcon.ts
+++ components/VscodeIcon.ts
@@ -1,55 +1,9 @@
 import { LitElement, html, svg, css, property, customElement } from 'lit-element';
-import getIcon from './getIcon';
-
-type Theme = 'light' | 'dark' | 'hc';
 
 @customElement('vscode-icon')
 export class VscodeIcon extends LitElement {
   @property({ type: String }) name: string;
   @property({ type: Number }) size: number = 16;
-  @property({ type: String })
-  public set theme(val: Theme) {
-    const oldVal = this._theme;
-
-    this._theme = val;
-    this.requestUpdate('theme', oldVal);
-  }
-  public get theme(): Theme {
-    return this._theme;
-  }
-
-  private _theme: Theme;
-
-  connectedCallback() {
-    super.connectedCallback();
-
-    if (!this.theme) {
-      this.setDefaultTheme();
-    }
-  }
-
-  private setDefaultTheme() {
-    if (document.body.classList.contains('theme-light')) {
-      this._theme = 'light';
-    } else if(document.body.classList.contains('theme-hc')) {
-      this._theme = 'hc';
-    } else {
-      this._theme = 'dark';
-    }
-  }
-
-  private getSize(): number {
-    return [
-      'file',
-      'search',
-      'source-control',
-      'debug',
-      'extensions',
-      'more',
-      'settings',
-      'references'
-    ].indexOf(this.name) !== -1 ? 24 : 16;
-  }
 
   static get styles() {
     return css`
@@ -58,30 +12,15 @@ export class VscodeIcon extends LitElement {
       }
 
       span {
+        background-position: center center;
+        background-repeat: no-repeat;
+        background-size: contain;
         display: block;
-      }
-
-      svg {
-        display: block;
-        height: 100%;
-        width: 100%;
       }
     `;
   }
 
   render() {
-    const size = this.getSize();
-    const shape = getIcon(this.name, this.theme);
-
-    const icon = svg`
-      <svg
-        viewBox="0 0 ${size} ${size}"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        ${shape}
-      </svg>
-    `;
 
     return html`
       <style>
@@ -90,13 +29,16 @@ export class VscodeIcon extends LitElement {
           width: ${this.size}px;
         }
 
-        :host-context([slot=actions]) {
-          margin: 0 4px;
+        :host-context(.vscode-light) {
+          background-image: url(icons/light/${this.name}.svg);
+        }
+
+        :host-context(.vscode-dark),
+        :host-context(.vscode-high-contrast) {
+          background-image: url(icons/dark/${this.name}.svg);
         }
       </style>
-      <span>
-        ${icon}
-      </span>
+      <span></span>
     `;
   }
 }
