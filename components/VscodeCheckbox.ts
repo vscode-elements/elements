5c805fc1a3c167ddb7dd11546e141818afc404d4
bendera <szoftverhiba@gmail.com>
2019-09-27 19:29:49 +0200

    checkbox improvements
    - add change event
    - add value prop
    - theming
    - optional label slot

--- components/VscodeCheckbox.ts
+++ components/VscodeCheckbox.ts
@@ -1,16 +1,26 @@
-import { LitElement, nothing, html, css, svg, property, customElement } from 'lit-element';
-import { classMap } from 'lit-html/directives/class-map';
-import { render } from 'lit-html/lib/shady-render';
+import { LitElement, html, css, svg, property, customElement } from 'lit-element';
+import { nothing } from 'lit-html';
 
 @customElement('vscode-checkbox')
 export class VscodeCheckbox extends LitElement {
   @property({ type: String }) label: string;
   @property({ type: Boolean }) checked: boolean = false;
+  @property({ type: String }) value: string;
 
   private _uid = `id_${new Date().valueOf()}_${Math.floor(Math.random() * 9999)}`;
 
   private onElementClick() {
     this.checked = !this.checked;
+
+    this.dispatchEvent(new CustomEvent('vsc-change', {
+      detail: {
+        checked: this.checked,
+        label: this.label,
+        value: this.value,
+      },
+      bubbles: true,
+      composed: true,
+    }));
   }
 
   static get styles() {
@@ -58,14 +68,26 @@ export class VscodeCheckbox extends LitElement {
         width: 16px;
       }
 
+      :host-context(.vscode-light) path {
+        fill: #424242;
+      }
+
+      :host-context(.vscode-dark) path,
+      :host-context(.vscode-high-contrast) path {
+        fill: #c5c5c5;
+      }
+
       .label {
+        padding-left: 27px;
+      }
+
+      .label-text {
         color: var(--vscode-breadcrumb-foreground);
         cursor: pointer;
         font-family: var(--vscode-font-family);
         font-size: var(--vscode-font-size);
         font-weight: var(--vscode-font-weight);
         line-height: 1.4;
-        padding-left: 27px;
       }
     `;
   };
@@ -73,7 +95,11 @@ export class VscodeCheckbox extends LitElement {
   render() {
     const icon = svg`
       <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" clip-rule="evenodd" d="M14.431 3.323l-8.47 10-.79-.036-3.35-4.77.818-.574 2.978 4.24 8.052-9.506.762.646z" fill="#424242"/>
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M14.431 3.323l-8.47 10-.79-.036-3.35-4.77.818-.574 2.978 4.24 8.052-9.506.762.646z"
+        />
       </svg>
     `;
 
@@ -81,10 +107,16 @@ export class VscodeCheckbox extends LitElement {
 
     return html`
       <div class="wrapper">
-        <input type="checkbox" id="${this._uid}" class="checkbox" ?checked="${this.checked}">
+        <input
+          id="${this._uid}"
+          class="checkbox"
+          type="checkbox"
+          ?checked="${this.checked}"
+          value="${this.value}"
+        >
         <div class="icon">${check}</div>
         <label for="${this._uid}" class="label" @click="${this.onElementClick}">
-          ${this.label}
+          <slot><span class="label-text">${this.label}</span></slot>
         </label>
       </div>
     `;
