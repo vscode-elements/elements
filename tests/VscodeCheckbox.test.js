7f25be2715dea7dd2f4864b3cdd702602c7246b4
bendera <szoftverhiba@gmail.com>
2019-10-11 21:14:55 +0200

    add tests

--- tests/VscodeCheckbox.test.js
+++ tests/VscodeCheckbox.test.js
@@ -13,4 +13,30 @@ describe('vscode-checkbox', () => {
     const tagName = await page.$eval('#checkbox-1', e => e.tagName);
     expect(tagName.toLowerCase()).toBe('vscode-checkbox');
   });
+
+  it('should not be checked', async () => {
+    const numIcons = await page.$eval('#checkbox-1', (e) => {
+      e.checked = false;
+      const icons = Array.from(e.querySelectorAll('.icon vscode-icon'));
+      return icons.length;
+    });
+
+    expect(numIcons).toBe(0);
+  });
+
+  it('should be checked', async () => {
+    const numIcons = await page.evaluate(async () => {
+      const cb = document.createElement('vscode-checkbox');
+      document.body.appendChild(cb);
+      cb.checked = true;
+
+      await cb.updateComplete;
+
+      const icons = Array.from(cb.shadowRoot.querySelectorAll('.icon vscode-icon'));
+
+      return Promise.resolve(icons.length);
+    });
+
+    expect(numIcons).toBe(1);
+  });
 });
