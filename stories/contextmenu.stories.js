6d6bc52f8d171ee3559f73f3d6e5fde089ec4ba2
bendera <szoftverhiba@gmail.com>
2019-10-14 18:46:13 +0200

    docs(ContextMenu): improve default story

--- stories/contextmenu.stories.js
+++ stories/contextmenu.stories.js
@@ -1,18 +1,84 @@
 import withVscodeThemes from './withVscodeThemes';
+import { withKnobs, boolean } from '@storybook/addon-knobs';
 
 export default {
   title: 'Context menu',
   decorators: [
+    withKnobs,
     withVscodeThemes,
   ]
 };
 
-export const defaultState = () => `
-  <vscode-context-menu>
-    <vscode-context-menu-item label="Lorem"></vscode-context-menu-item>
-    <vscode-context-menu-item label="Ipsum"></vscode-context-menu-item>
-  </vscode-context-menu>
-`;
+export const defaultState = () => {
+  const menu = document.createElement('vscode-context-menu');
+
+  const data = [
+    {
+      label: 'Command Palette...',
+      keybinding: 'Ctrl+Shift+P',
+      value: 'Custom value',
+      tabindex: '0',
+    },
+    {
+      separator: true,
+    },
+    {
+      label: 'Settings',
+      keybinding: 'Ctrl+,',
+    },
+    {
+      label: 'Online Services Settings',
+    },
+    {
+      label: 'Extensions',
+      keybinding: 'Ctrl+Shift+X',
+    },
+    {
+      separator: true,
+    },
+    {
+      label: 'Keyboard shortcuts',
+      keybinding: 'Ctrl+K Ctrl+S',
+    },
+    {
+      label: 'Keymaps',
+      keybinding: 'Ctrl+K Ctrl+M',
+    },
+    {
+      separator: true,
+    },
+    {
+      label: 'User snippets',
+    },
+    {
+      separator: true,
+    },
+    {
+      label: 'Color Theme',
+      keybinding: 'Ctrl+K Ctrl+T',
+    },
+    {
+      label: 'File Icon Theme',
+    },
+    {
+      separator: true,
+    },
+    {
+      label: 'Check for Updates...',
+    },
+  ];
+
+  menu.data = data;
+  menu.style.display = 'flex';
+  menu.show = boolean('show', true);
+
+  menu.addEventListener('vsc-select', (event) => {
+    console.log(event);
+  });
+
+  return menu;
+};
+
 defaultState.story = {
   name: 'Default',
 };
