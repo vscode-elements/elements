9e814cb7a019f617f03755e1fd445ed1eb207677
bendera <szoftverhiba@gmail.com>
2021-01-20 22:36:48 +0100

    feat(checkbox): disabled state

--- src/vscode-checkbox.ts
+++ src/vscode-checkbox.ts
@@ -1,4 +1,11 @@
-import {LitElement, html, css, property, customElement, CSSResult} from 'lit-element';
+import {
+  LitElement,
+  html,
+  css,
+  property,
+  customElement,
+  CSSResult,
+} from 'lit-element';
 import {nothing, TemplateResult} from 'lit-html';
 
 @customElement('vscode-checkbox')
@@ -7,17 +14,35 @@ export class VscodeCheckbox extends LitElement {
   @property({type: Boolean}) checked = false;
   @property() value = '';
   @property({type: Number, reflect: true}) tabindex = 0;
+  @property({type: Boolean}) disabled = false;
 
   constructor() {
     super();
     this.addEventListener('keydown', this._handleKeyDown.bind(this));
   }
 
+  attributeChangedCallback(name: string, oldVal: string, newVal: string): void {
+    super.attributeChangedCallback(name, oldVal, newVal);
+
+    if (name === 'disabled' && this.hasAttribute('disabled')) {
+      this._prevTabindex = this.tabindex;
+      this.tabindex = -1;
+    } else if (name === 'disabled' && !this.hasAttribute('disabled')) {
+      this.tabindex = this._prevTabindex;
+    }
+  }
+
+  private _prevTabindex = 0;
+
   private _uid = `id_${new Date().valueOf()}_${Math.floor(
     Math.random() * 9999
   )}`;
 
   private _handleClick() {
+    if (this.disabled) {
+      return;
+    }
+
     this.checked = !this.checked;
 
     this.dispatchEvent(
@@ -34,7 +59,7 @@ export class VscodeCheckbox extends LitElement {
   }
 
   private _handleKeyDown(event: KeyboardEvent) {
-    if (event.key === 'Enter' || event.key === ' ') {
+    if (!this.disabled && (event.key === 'Enter' || event.key === ' ')) {
       this.checked = !this.checked;
     }
   }
@@ -49,12 +74,20 @@ export class VscodeCheckbox extends LitElement {
         outline: none;
       }
 
+      :host([disabled]) {
+        opacity: 0.4;
+      }
+
       .wrapper {
         cursor: pointer;
         display: block;
         font-size: var(--vscode-font-size);
         position: relative;
         user-select: none;
+      }
+
+      :host([disabled]) .wrapper {
+        cursor: default;
       }
 
       .checkbox {
@@ -83,7 +116,7 @@ export class VscodeCheckbox extends LitElement {
         width: 18px;
       }
 
-      :host(:focus) .icon {
+      :host(:focus):host(:not([disabled])) .icon {
         outline: 1px solid var(--vscode-focusBorder);
         outline-offset: -1px;
       }
@@ -99,6 +132,10 @@ export class VscodeCheckbox extends LitElement {
         font-size: var(--vscode-font-size);
         font-weight: var(--vscode-font-weight);
         line-height: 1.4;
+      }
+
+      :host([disabled]) .label-text {
+        cursor: default;
       }
     `;
   }
