627e9ef76a8e172e5e49bb840c9d1d73b6dd01fc
bendera <szoftverhiba@gmail.com>
2021-04-08 21:14:22 +0200

    test: update multiselect tests

--- src/test/vscode-multi-select.test.ts
+++ src/test/vscode-multi-select.test.ts
@@ -1,11 +1,33 @@
 import {VscodeMultiSelect} from '../vscode-select/vscode-multi-select';
 import {expect, fixture, html} from '@open-wc/testing';
-import sinon from 'sinon';
 import '../vscode-select/vscode-option';
 
 describe('vscode-multi-select', () => {
   it('is defined', () => {
     const el = document.createElement('vscode-multi-select');
     expect(el).to.instanceOf(VscodeMultiSelect);
+  });
+
+  it('should display selected value', async () => {
+    const el = (await fixture(html`
+      <vscode-multi-select>
+        <vscode-option>Lorem</vscode-option>
+        <vscode-option selected>Ipsum</vscode-option>
+        <vscode-option>Dolor</vscode-option>
+      </vscode-multi-select>
+    `)) as VscodeMultiSelect;
+
+    expect(el).shadowDom.to.equal(`
+      <slot class="main-slot"></slot>
+      <div class="multiselect select-face">
+        <span class="select-face-badge">
+          1 item selected
+        </span>
+        <span class="icon">
+        </span>
+      </div>
+    `);
+    expect(el.selectedIndexes).to.eql([1]);
+    expect(el.value).to.eql(['Ipsum']);
   });
 });
