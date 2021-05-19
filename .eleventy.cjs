b4b41c4ad7a109763d601b69cc7f3a1028382b49
bendera <szoftverhiba@gmail.com>
2021-05-19 23:08:42 +0200

    docs: custom browsersync config

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -39,6 +39,11 @@ module.exports = function (eleventyConfig) {
       });
   });
 
+  eleventyConfig.setBrowserSyncConfig({
+    notify: true,
+    open: true,
+  });
+
   let conf = {
     dir: {
       input: 'docs-src',
