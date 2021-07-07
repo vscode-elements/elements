be782f8fbdf78730dc8d1afae9158304e8a4f5b9
bendera <szoftverhiba@gmail.com>
2021-07-07 21:20:21 +0200

    remove unused parts

--- src/vscode-radio.ts
+++ src/vscode-radio.ts
@@ -7,7 +7,7 @@ import {
   CSSResult,
   state,
 } from 'lit-element';
-import {nothing, TemplateResult} from 'lit-html';
+import {TemplateResult} from 'lit-html';
 import {classMap} from 'lit-html/directives/class-map';
 
 @customElement('vscode-radio')
@@ -199,20 +199,6 @@ export class VscodeRadio extends LitElement {
   }
 
   render(): TemplateResult {
-    const icon = html`<svg
-      width="16"
-      height="16"
-      viewBox="0 0 16 16"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="currentColor"
-    >
-      <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
-        d="M14.431 3.323l-8.47 10-.79-.036-3.35-4.77.818-.574 2.978 4.24 8.051-9.506.764.646z"
-      />
-    </svg>`;
-    const check = this._checked ? icon : nothing;
     const iconClasses = classMap({
       icon: true,
       checked: this.checked,
