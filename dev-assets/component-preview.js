487b065a77b34b914561b03cbd0ce1033ccd1186
bendera <szoftverhiba@gmail.com>
2021-07-13 19:16:06 +0200

    docs: component preview improvements

--- dev-assets/component-preview.js
+++ dev-assets/component-preview.js
@@ -29,6 +29,12 @@ tmpl.innerHTML = `
   <style>
     :host {
       all: initial;
+      display: block;
+      margin: 32px 0;
+    }
+
+    .canvas {
+      all: initial;
       background-color: var(--vscode-editor-background);
       color: var(--vscode-foreground);
       display: block;
@@ -39,32 +45,70 @@ tmpl.innerHTML = `
     }
 
     .theme-selector-wrapper {
-      margin-bottom: 20px;
+      position: relative;
     }
 
     .theme-selector {
       background-color: var(--vscode-editor-background);
-      border: 1px solid var(--vscode-foreground);
-      border-radius: 0;
-      color: var(--vscode-foreground);
-      font-family: var(--vscode-font-family);
-      font-size: var(--vscode-font-size);
+      box-sizing: border-box;
+      display: flex;
+      padding: 2px 20px 0;
+      position: relative;
+      width: 100%;
+      z-index: 2;
     }
 
-    .theme-selector:focus {
-      outline: 1px solid var(--vscode-foreground);
+    .theme-selector:after {
+      background-color: var(--vscode-foreground);
+      bottom: 0;
+      content: '';
+      display: block;
+      height: 1px;
+      left: 0;
+      opacity: 0.2;
+      pointer-events: none;
+      position: absolute;
+      width: 100%;
+    }
+
+    .theme-selector button {
+      background-color: transparent;
+      border: 0;
+      border-bottom: 1px solid transparent;
+      cursor: pointer;
+      color: var(--vscode-foreground);
+      display: block;
+      margin-right: 20px;
+      outline: none;
+      padding: 5px 0;
+    }
+
+    .theme-selector button.active {
+      border-bottom-color: var(--vscode-foreground);
+    }
+
+    .theme-selector button span {
+      display: block;
+      outline-offset: 2px;
+      pointer-events: none;
+    }
+
+    .theme-selector button:focus-visible span {
+      outline: 1px solid var(--vscode-focusBorder);
     }
   </style>
   <div class="theme-selector-wrapper">
-    <select id="theme-selector" class="theme-selector">
-      <option value="light">Light+ (default light)</option>
-      <option value="dark">Dark+ (default dark)</option>
-      <option value="high-contrast">High Contrast</option>
-      <option value="github-light">Github Light</option>
-      <option value="one-dark-pro">One Dark Pro</option>
-    </select>
+    <div id="theme-selector" class="theme-selector">
+      <button type="button" value="light" class="active"><span>Light</span></button>
+      <button type="button" value="dark"><span>Dark</span></button>
+      <button type="button" value="high-contrast"><span>High Contrast</span></button>
+      <button type="button" value="github-light"><span>GitHub Light</span></button>
+      <button type="button" value="one-dark-pro"><span>One Dark Pro</span></button>
+    </div>
   </div>
-  <slot></slot>
+  <div class="canvas">
+    <slot></slot>
+  </div>
 `;
 
 class ComponentPreview extends HTMLElement {
@@ -75,27 +119,31 @@ class ComponentPreview extends HTMLElement {
     shadowRoot.appendChild(tmpl.content.cloneNode(true));
 
     this._elThemeSelector = shadowRoot.querySelector('.theme-selector');
+    this._elButtons = this._elThemeSelector.querySelectorAll('button');
+
     instanceCounter++;
     themeSelectorInstances[
       `instance-${instanceCounter}`
     ] = this._elThemeSelector;
-    this._onThemeSelectorChangeBound = this._onThemeSelectorChange.bind(this);
-    this._elThemeSelector.addEventListener(
-      'change',
-      this._onThemeSelectorChangeBound
+
+    this._onThemeSelectorButtonClickBound = this._onThemeSelectorButtonClick.bind(
+      this
     );
+
+    this._elButtons.forEach((b) => {
+      b.addEventListener('click', this._onThemeSelectorButtonClickBound);
+    });
 
     this._applyTheme('light');
   }
 
-  _onThemeSelectorChange() {
-    this._runOperationOnEachThemeSelector(
-      'setValue',
-      this._elThemeSelector.value
-    );
+  _onThemeSelectorButtonClick(ev) {
+    const value = ev.target.value;
+
+    this._runOperationOnEachThemeSelector('setValue', value);
     this._runOperationOnEachThemeSelector('disable');
 
-    this._applyTheme(this._elThemeSelector.value).then(() => {
+    this._applyTheme(value).then(() => {
       this._runOperationOnEachThemeSelector('enable');
     });
   }
@@ -106,13 +154,19 @@ class ComponentPreview extends HTMLElement {
     instanceKeys.forEach((k) => {
       switch (command) {
         case 'enable':
-          themeSelectorInstances[k].disabled = false;
+          themeSelectorInstances[k].querySelectorAll('button').forEach((b) => {
+            b.disabled = false;
+          });
           break;
         case 'disable':
-          themeSelectorInstances[k].disabled = true;
+          themeSelectorInstances[k].querySelectorAll('button').forEach((b) => {
+            b.disabled = true;
+          });
           break;
         case 'setValue':
-          themeSelectorInstances[k].value = args[0];
+          themeSelectorInstances[k].querySelectorAll('button').forEach((b) => {
+            b.classList.toggle('active', b.value === args[0]);
+          });
           break;
         default:
       }
