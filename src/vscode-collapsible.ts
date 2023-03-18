19d00702db22dd78240e83950c79e0c8e736722b
bendera <szoftverhiba@gmail.com>
2023-03-18 09:09:49 +0100

    feat: Collapsible fallback css

--- src/vscode-collapsible.ts
+++ src/vscode-collapsible.ts
@@ -1,18 +1,19 @@
 import {css, CSSResultGroup, html, TemplateResult} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 import {classMap} from 'lit/directives/class-map.js';
+import declareThemeVariables from './includes/declareThemeVariables';
 import {VscElement} from './includes/VscElement';
 
 /**
  * @slot body - Main content
  * @slot actions - Action icons in the header
  *
- * @cssprop [--font-family=var(--vscode-font-family)]
  * @cssprop [--background=var(--vscode-sideBar-background)]
- * @cssprop [--header-background=var(--vscode-sideBarSectionHeader-background)]
  * @cssprop [--focus-border=var(--vscode-focusBorder)]
- * @cssprop [--title-foreground=var(--vscode-sideBarTitle-foreground)]
+ * @cssprop [--font-family=var(--vscode-font-family)]
+ * @cssprop [--header-background=var(--vscode-sideBarSectionHeader-background)]
  * @cssprop [--icon-foreground=var(--vscode-icon-foreground)]
+ * @cssprop [--title-foreground=var(--vscode-sideBarTitle-foreground)]
  */
 @customElement('vscode-collapsible')
 export class VscodeCollapsible extends VscElement {
@@ -35,49 +36,33 @@ export class VscodeCollapsible extends VscElement {
   static get styles(): CSSResultGroup {
     return [
       super.styles,
+      declareThemeVariables([
+        {
+          componentProp: '--background',
+          vscodeProp: '--vscode-sideBar-background',
+        },
+        {
+          componentProp: '--focus-border',
+          vscodeProp: '--vscode-focusBorder',
+        },
+        {
+          componentProp: '--font-family',
+          vscodeProp: '--vscode-font-family',
+        },
+        {
+          componentProp: '--header-background',
+          vscodeProp: '--vscode-sideBarSectionHeader-background',
+        },
+        {
+          componentProp: '--icon-foreground',
+          vscodeProp: '--vscode-icon-foreground',
+        },
+        {
+          componentProp: '--title-foreground',
+          vscodeProp: '--vscode-sideBarTitle-foreground',
+        },
+      ]),
       css`
-        :host {
-          --font-family: var(
-            --vscode-font-family,
-            'Segoe WPC',
-            'Segoe UI',
-            sans-serif
-          );
-        }
-
-        :host-context(body.vscode-dark) {
-          --background: var(--vscode-sideBar-background, #252526);
-          --header-background: var(
-            --vscode-sideBarSectionHeader-background,
-            #000000
-          );
-          --focus-border: var(--vscode-focusBorder, #007fd4);
-          --title-foreground: var(--vscode-sideBarTitle-foreground, #bbbbbb);
-          --icon-foreground: var(--vscode-icon-foreground, #c5c5c5);
-        }
-
-        :host-context(body.vscode-light) {
-          --background: var(--vscode-sideBar-background, #f3f3f3);
-          --header-background: var(
-            --vscode-sideBarSectionHeader-background,
-            rgba(0, 0, 0, 0)
-          );
-          --focus-border: var(--vscode-focusBorder, #0090f1);
-          --title-foreground: var(--vscode-sideBarTitle-foreground, #6f6f6f);
-          --icon-foreground: var(--vscode-icon-foreground, #424242);
-        }
-
-        :host-context(body.vscode-high-contrast) {
-          --background: var(--vscode-sideBar-background, #000000);
-          --header-background: var(
-            --vscode-sideBarSectionHeader-background,
-            #000000
-          );
-          --focus-border: var(--vscode-focusBorder, #f38518);
-          --title-foreground: var(--vscode-sideBarTitle-foreground, #ffffff);
-          --icon-foreground: var(--vscode-icon-foreground, #ffffff);
-        }
-
         .collapsible {
           background-color: var(--background);
         }
