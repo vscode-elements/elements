758a1f9273f2ca18668fb8807bb4e86ebc2b12ca
bendera <szoftverhiba@gmail.com>
2023-06-17 22:26:02 +0200

    Setup rollup 3

--- rollup.config.js
+++ rollup.config.js
@@ -7,8 +7,44 @@
 import {terser} from 'rollup-plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
-import visualizer from 'rollup-plugin-visualizer';
-import minifyHTML from 'rollup-plugin-minify-html-literals';
+import {visualizer} from 'rollup-plugin-visualizer';
+import * as minify from 'minify-html-literals';
+import {createFilter} from 'rollup-pluginutils';
+
+function minifyHTML(options = {}) {
+  if (!options.minifyHTMLLiterals) {
+    options.minifyHTMLLiterals = minify.minifyHTMLLiterals;
+  }
+
+  if (!options.filter) {
+    options.filter = createFilter(options.include, options.exclude);
+  }
+
+  const minifyOptions = options.options || {};
+
+  return {
+    name: 'minify-html-literals',
+    transform(pluginContext, code, id) {
+      if (options.filter(id)) {
+        try {
+          return options.minifyHTMLLiterals(code, {
+            ...minifyOptions,
+            fileName: id,
+          });
+        } catch (error) {
+          // check if Error ese treat as string
+          const message = error instanceof Error ? error.message : error;
+
+          if (options.failOnError) {
+            pluginContext.error(message);
+          } else {
+            pluginContext.warn(message);
+          }
+        }
+      }
+    },
+  };
+}
 
 export default {
   input: 'dist/main.js',
