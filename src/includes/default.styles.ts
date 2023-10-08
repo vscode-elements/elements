e480c464a197a2c2f869bc12ac2553bcdd5fbed5
bendera <szoftverhiba@gmail.com>
2023-10-09 01:14:54 +0200

    Default disabled styles

--- src/includes/default.styles.ts
+++ src/includes/default.styles.ts
@@ -4,4 +4,11 @@ export default css`
   :host([hidden]) {
     display: none;
   }
+
+  :host([disabled]),
+  :host(:disabled) {
+    cursor: not-allowed;
+    opacity: 0.4;
+    pointer-events: none;
+  }
 `;
