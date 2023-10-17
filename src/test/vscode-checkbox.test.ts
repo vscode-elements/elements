8866933e3f2cd708a14be3ee49b097d3369ff229
bendera <szoftverhiba@gmail.com>
2023-10-17 17:37:55 +0200

    Update tests

--- src/test/vscode-checkbox.test.ts
+++ src/test/vscode-checkbox.test.ts
@@ -1,5 +1,6 @@
 import {VscodeCheckbox} from '../vscode-checkbox/index.js';
 import {expect, fixture, html} from '@open-wc/testing';
+import sinon from 'sinon';
 
 describe('vscode-checkbox', () => {
   it('is defined', () => {
@@ -57,6 +58,77 @@ describe('vscode-checkbox', () => {
     expect(value).to.eq('on');
   });
 
+  it('should return the form in which participated', () => {
+    const form = document.createElement('form');
+    const el = document.createElement('vscode-checkbox') as VscodeCheckbox;
+    form.appendChild(el);
+
+    expect(el.form).to.instanceOf(HTMLFormElement);
+  });
+
+  it('should return the validity object', () => {
+    const el = document.createElement('vscode-checkbox') as VscodeCheckbox;
+
+    expect(el.validity).to.instanceOf(ValidityState);
+  });
+
+  it('should return the validation message', async () => {
+    const form = document.createElement('form');
+    const el = document.createElement('vscode-checkbox');
+    el.setAttribute('required', '');
+    form.appendChild(el);
+    document.body.appendChild(form);
+
+    await el.updateComplete;
+
+    expect(el.validationMessage).to.not.empty;
+    expect(el.validationMessage).to.eq(
+      'Please check this box if you want to proceed.'
+    );
+  });
+
+  it('should willValidate property be true if the element is candidate for constraint validation', async () => {
+    const el = await fixture<VscodeCheckbox>(
+      '<vscode-checkbox></vscode-checkbox>'
+    );
+
+    expect(el.willValidate).to.be.true;
+  });
+
+  it('should willValidate property be false if the element is not candidate for constraint validation', async () => {
+    const el = await fixture<VscodeCheckbox>(
+      '<vscode-checkbox disabled></vscode-checkbox>'
+    );
+
+    expect(el.willValidate).to.be.false;
+  });
+
+  it('should check validity when checkValidity is called', async () => {
+    const el = await fixture<VscodeCheckbox>(
+      '<vscode-checkbox required></vscode-checkbox>'
+    );
+    const fn = sinon.spy();
+    el.addEventListener('invalid', fn);
+
+    const result = el.checkValidity();
+
+    expect(result).to.be.false;
+    expect(fn.called).to.be.true;
+  });
+
+  it('reportValidity should be called', async () => {
+    const el = await fixture<VscodeCheckbox>(
+      '<vscode-checkbox required></vscode-checkbox>'
+    );
+    const fn = sinon.spy();
+    el.addEventListener('invalid', fn);
+
+    const result = el.reportValidity();
+
+    expect(result).to.be.false;
+    expect(fn.called).to.be.true;
+  });
+
   it('the autofocus attribute should be passed', async () => {
     const el = await fixture<VscodeCheckbox>(
       html`<vscode-checkbox name="test" autofocus></vscode-checkbox>`
@@ -78,6 +150,19 @@ describe('vscode-checkbox', () => {
     const initialChecked = el.checked;
 
     form.reset();
+    await el.updateComplete;
+
+    expect(initialChecked).to.be.false;
+    expect(el.checked).to.be.true;
+  });
+
+  it('restore callback should restore the previous state', async () => {
+    const el = await fixture<VscodeCheckbox>(
+      '<vscode-checkbox value="test"></vscode-checkbox>'
+    );
+    const initialChecked = el.checked;
+
+    el.formStateRestoreCallback('test', 'restore');
     await el.updateComplete;
 
     expect(initialChecked).to.be.false;
