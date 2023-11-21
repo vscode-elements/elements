cf0ff9ba1e10fa2943289a0de4952734a4454fc6
bendera <szoftverhiba@gmail.com>
2023-11-21 17:06:03 +0100

    Mark vsc-click as deprecated

--- src/vscode-button/vscode-button.ts
+++ src/vscode-button/vscode-button.ts
@@ -129,7 +129,10 @@ export class VscodeButton extends VscElement {
       (event.key === 'Enter' || event.key === ' ') &&
       !this.hasAttribute('disabled')
     ) {
-      /** @deprecated */
+      /**
+       * @deprecated
+       * Please use the standard `click` event.
+       */
       this.dispatchEvent(
         new CustomEvent<{
           originalEvent: MouseEvent;
