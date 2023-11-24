a6e12e28daa41f4b1821a9e7d87e9fca87cdb855
bendera <szoftverhiba@gmail.com>
2023-11-24 16:39:10 +0100

    Add version info to the base class

--- src/includes/VscElement.ts
+++ src/includes/VscElement.ts
@@ -1,3 +1,6 @@
 import {LitElement} from 'lit';
 
-export class VscElement extends LitElement {}
+export class VscElement extends LitElement {
+  /** VSC Element version */
+  public readonly version = '1.0.0-pre.5';
+}
