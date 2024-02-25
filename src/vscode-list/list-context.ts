3cff111c1a0a8028e6e184f3d7355a9cfb6df70a
bendera <szoftverhiba@gmail.com>
2024-02-25 14:53:50 +0100

    Add ability to select items

--- src/vscode-list/list-context.ts
+++ src/vscode-list/list-context.ts
@@ -1,8 +1,10 @@
 import {createContext} from '@lit/context';
+import {type VscodeListItem} from '../vscode-list-item';
 
 export interface ListContext {
   indent: number;
   arrows: boolean;
+  selectedItems: Set<VscodeListItem>;
 }
 
 export const listContext = createContext<ListContext>('vscode-list');
