083a7508d7c788182f8449979036e3db89b2c3c8
bendera <szoftverhiba@gmail.com>
2024-09-12 21:14:24 +0200

    Add tests

--- src/includes/vscode-select/helpers.test.ts
+++ src/includes/vscode-select/helpers.test.ts
@@ -1,39 +1,48 @@
-import {expect} from '@open-wc/testing';
+import {expect, fixture, html} from '@open-wc/testing';
 import {
-  fuzzyMatch,
-  startsWithPerTermMatch,
-  startsWithMatch,
-  containsMatch,
+  containsSearch,
+  fuzzySearch,
+  highlightRanges,
   startsWithPerTermSearch,
+  startsWithSearch,
 } from './helpers.js';
 
 describe('helpers', () => {
-  it('fuzzyMatch', () => {
-    expect(fuzzyMatch('Lorem ipsum', 'ri')).to.eq(true);
-    expect(fuzzyMatch('Lorem ipsum', 'um')).to.eq(true);
-    expect(fuzzyMatch('Lorem ipsum', 'io')).to.eq(false);
+  it('fuzzySearch should match', () => {
+    const result = fuzzySearch(
+      "I think Ruth's dog is cuter than your dog!",
+      'hnu'
+    );
+
+    expect(result.match).to.eq(true);
+    expect(result.ranges).to.eql([
+      [3, 4],
+      [5, 6],
+      [9, 10],
+    ]);
   });
 
-  it('startsWithPerTermMatch', () => {
-    expect(startsWithPerTermMatch('Lorem ipsum', 'lo')).to.eq(true);
-    expect(startsWithPerTermMatch('Lorem ipsum', 'ip')).to.eq(true);
-    expect(startsWithPerTermMatch('Lorem ipsum', 'or')).to.eq(false);
+  it('fuzzySearch should not match', () => {
+    const result = fuzzySearch(
+      "I think Ruth's dog is cuter than your dog!",
+      'ruk'
+    );
+
+    expect(result.match).to.eq(false);
+    expect(result.ranges).to.eql([]);
   });
 
-  it('startsWithPerTermSearch', () => {
+  it('startsWithPerTermSearch should match', () => {
     const result = startsWithPerTermSearch(
       "I think Ruth's dog is cuter than your dog!",
       'dog'
     );
 
     expect(result.match).to.eq(true);
-    expect(result.ranges).to.eql([
-      [15, 18],
-      [38, 41],
-    ]);
+    expect(result.ranges).to.eql([[15, 18]]);
   });
 
-  it('startsWithPerTermSearch 2', () => {
+  it('startsWithPerTermSearch should not match', () => {
     const result = startsWithPerTermSearch(
       "I think Ruth's dog is cuter than your dog!",
       'uth'
@@ -43,13 +52,83 @@ describe('helpers', () => {
     expect(result.ranges).to.eql([]);
   });
 
-  it('startsWithPerTermMatch', () => {
-    expect(startsWithMatch('Lorem ipsum', 'lo')).to.eq(true);
-    expect(startsWithMatch('Lorem ipsum', 'ip')).to.eq(false);
+  it('containsSearch should match', () => {
+    const result = containsSearch(
+      "I think Ruth's dog is cuter than your dog!",
+      'uth'
+    );
+
+    expect(result.match).to.eq(true);
+    expect(result.ranges).to.eql([[9, 12]]);
   });
 
-  it('containsMatch', () => {
-    expect(containsMatch('Lorem ipsum', 're')).to.eq(true);
-    expect(containsMatch('Lorem ipsum', 'aa')).to.eq(false);
+  it('containsSearch should not match', () => {
+    const result = containsSearch(
+      "I think Ruth's dog is cuter than your dog!",
+      'htu'
+    );
+
+    expect(result.match).to.eq(false);
+    expect(result.ranges).to.eql([]);
+  });
+
+  it('startsWithSearch should match', () => {
+    const result = startsWithSearch(
+      "I think Ruth's dog is cuter than your dog!",
+      'I think'
+    );
+
+    expect(result.match).to.eq(true);
+    expect(result.ranges).to.eql([[0, 7]]);
+  });
+
+  it('startsWithSearch should not match', () => {
+    const result = startsWithSearch(
+      "I think Ruth's dog is cuter than your dog!",
+      'think'
+    );
+
+    expect(result.match).to.eq(false);
+    expect(result.ranges).to.eql([]);
+  });
+
+  describe('highlightRanges', () => {
+    it('should highlight ranges', async () => {
+      const el = await fixture(
+        html`<span
+          >${highlightRanges('lorem ipsum dolor', [
+            [1, 4],
+            [7, 10],
+            [13, 16],
+          ])}</span
+        >`
+      );
+
+      expect(el).lightDom.to.equals('l<b>ore</b>m i<b>psu</b>m d<b>olo</b>r');
+    });
+
+    it('should highlight range correctly at start', async () => {
+      const el = await fixture(
+        html`<span>${highlightRanges('lorem ipsum dolor', [[0, 2]])}</span>`
+      );
+
+      expect(el).lightDom.to.equals('<b>lo</b>rem ipsum dolor');
+    });
+
+    it('should highlight range correctly at end', async () => {
+      const el = await fixture(
+        html`<span>${highlightRanges('lorem ipsum dolor', [[15, 17]])}</span>`
+      );
+
+      expect(el).lightDom.to.equals('lorem ipsum dol<b>or</b>');
+    });
+
+    it('should render spaces correctly', async () => {
+      const el = await fixture(
+        html`<span>${highlightRanges('lorem ipsum dolor', [[5, 17]])}</span>`
+      );
+
+      expect(el).lightDom.to.equals('lorem<b> ipsum dolor</b>');
+    });
   });
 });
