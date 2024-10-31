0a3b67a403c2ee85a5e55a38479318a037c83c63
bendera <szoftverhiba@gmail.com>
2024-10-31 01:06:46 +0100

    Add content wrapper to more error proof calculations

--- src/vscode-split-layout/vscode-split-layout.styles.ts
+++ src/vscode-split-layout/vscode-split-layout.styles.ts
@@ -7,7 +7,7 @@ const styles: CSSResultGroup = [
     :host {
       --separator-border: var(--vscode-editorWidget-border, transparent);
 
-      /* border: 1px solid var(--vscode-editorWidget-border); */
+      border: 1px solid var(--vscode-editorWidget-border);
       display: block;
       overflow: hidden;
       position: relative;
@@ -20,6 +20,11 @@ const styles: CSSResultGroup = [
 
     ::slotted(vscode-split-layout) {
       border: 0;
+    }
+
+    .wrapper {
+      height: 100%;
+      width: 100%;
     }
 
     .start {
