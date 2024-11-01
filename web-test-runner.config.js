fd5e1601a13fc9976cadce02e5c6fe4653927219
bendera <szoftverhiba@gmail.com>
2024-11-01 01:35:03 +0100

    Eliminate message log pollution

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -2,4 +2,8 @@ export default {
   files: ['./dist/**/*.test.js'],
   nodeResolve: true,
   preserveSymlinks: true,
+  filterBrowserLogs: ({args}) =>
+    !args[0].startsWith(
+      'Lit is in dev mode. Not recommended for production!'
+    ) && !args[0].includes('https://lit.dev/msg/change-in-update'),
 };
