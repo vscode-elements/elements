2fef1ad5fccb7b1caebf192bd42e34900fdcbb17
bendera <szoftverhiba@gmail.com>
2024-11-03 23:04:17 +0100

    Fix browser logs filter in WTR config

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -6,7 +6,7 @@ export default {
   preserveSymlinks: true,
   browsers: [playwrightLauncher({product: 'chromium'})],
   filterBrowserLogs: ({args}) =>
-    !args[0].startsWith(
+    !args[0]?.startsWith(
       'Lit is in dev mode. Not recommended for production!'
-    ) && !args[0].includes('https://lit.dev/msg/change-in-update'),
+    ) && !args[0]?.includes('https://lit.dev/msg/change-in-update'),
 };
