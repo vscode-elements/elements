2875f93c61f80461b70f8e0233b667bceba68b2d
bendera <szoftverhiba@gmail.com>
2024-11-03 23:02:20 +0100

    Use playwright instead of puppeteer
    Async timeout error cannot be fixed in puppeteer

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,7 +1,10 @@
+import { playwrightLauncher } from '@web/test-runner-playwright';
+
 export default {
   files: ['./dist/**/*.test.js'],
   nodeResolve: true,
   preserveSymlinks: true,
+  browsers: [playwrightLauncher({product: 'chromium'})],
   filterBrowserLogs: ({args}) =>
     !args[0].startsWith(
       'Lit is in dev mode. Not recommended for production!'
