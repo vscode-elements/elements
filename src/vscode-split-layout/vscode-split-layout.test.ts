047a3e73074be6eb9f96c9064a0e0942506f7886
bendera <szoftverhiba@gmail.com>
2024-11-04 23:12:19 +0100

    Update tests

--- src/vscode-split-layout/vscode-split-layout.test.ts
+++ src/vscode-split-layout/vscode-split-layout.test.ts
@@ -3,6 +3,7 @@ import {expect, fixture, html} from '@open-wc/testing';
 import {resetMouse} from '@web/test-runner-commands';
 import {parseValue} from './vscode-split-layout.js';
 import {dragElement} from '../includes/test-helpers.js';
+import sinon from 'sinon';
 
 describe('vscode-split-layout', () => {
   afterEach(async () => {
@@ -29,6 +30,11 @@ describe('vscode-split-layout', () => {
 
     it('should parse non-number value correctly', () => {
       expect(parseValue('asdf')).to.deep.equal({unit: 'pixel', value: 0});
+    });
+
+    it('should handle null input value correctly', () => {
+      // @ts-expect-error
+      expect(parseValue(null)).to.deep.equal({unit: 'pixel', value: 0});
     });
   });
 
@@ -102,9 +108,6 @@ describe('vscode-split-layout', () => {
       expect(handle.style.left).to.eq('0px');
       expect(handle.style.top).to.eq('50%');
     });
-
-    it('should set vertical handle size');
-    it('should set horizontal handle size');
   });
 
   describe('user interactions', () => {
@@ -117,7 +120,9 @@ describe('vscode-split-layout', () => {
       );
 
       const handle = el.shadowRoot?.querySelector('.handle') as HTMLDivElement;
-      const paneStart = el.shadowRoot?.querySelector('.start') as HTMLDivElement;
+      const paneStart = el.shadowRoot?.querySelector(
+        '.start'
+      ) as HTMLDivElement;
       const paneEnd = el.shadowRoot?.querySelector('.end') as HTMLDivElement;
 
       expect(handle).not.to.be.null;
@@ -154,5 +159,65 @@ describe('vscode-split-layout', () => {
       expect(handle.style.left).to.eq('0px');
       expect(handle.style.top).to.eq('40%');
     });
+
+    it('should dispatch "vsc-split-layout-change" event when handle position is changed', async () => {
+      const el = await fixture<VscodeSplitLayout>(
+        html`<vscode-split-layout
+          style="width: 500px; height: 500px;"
+          initial-handle-position="100px"
+          split="vertical"
+        ></vscode-split-layout>`
+      );
+      const spy = sinon.spy();
+      el.addEventListener('vsc-split-layout-change', spy);
+
+      const handle = el.shadowRoot?.querySelector('.handle') as HTMLDivElement;
+      await dragElement(handle, 100);
+
+      expect(
+        spy.calledWithMatch({
+          type: 'vsc-split-layout-change',
+          detail: {position: 200, positionInPercentage: 40},
+        })
+      ).to.be.true;
+    });
+
+    it('should reset handle position on double click when "reset-on-dbl-click" is set', async () => {
+      const el = await fixture<VscodeSplitLayout>(
+        html`<vscode-split-layout
+          style="width: 500px; height: 500px;"
+          initial-handle-position="100px"
+          reset-on-dbl-click
+          split="vertical"
+        ></vscode-split-layout>`
+      );
+
+      const handle = el.shadowRoot?.querySelector('.handle') as HTMLDivElement;
+      const initialHandlePos = handle.style.left;
+      await dragElement(handle, 100);
+
+      const changedHandlePos = handle.style.left;
+      handle.dispatchEvent(new MouseEvent('dblclick'));
+      await el.updateComplete;
+
+      const revertedHandlePos = handle.style.left;
+
+      expect(initialHandlePos).to.eq('20%');
+      expect(changedHandlePos).to.eq('40%');
+      expect(revertedHandlePos).to.eq('20%');
+    });
+
+    it('should not reset handle position on double click when "reset-on-dbl-click" is unset');
   });
+
+  // TODO
+  it('should set vertical handle size');
+  it('should set horizontal handle size');
+  it('should change divider orientation from vertical to horizontal');
+  it('should change divider orientation from horizontal to vertical');
+  it('should set fixed start panel');
+  it('should set fixed end panel');
+  it('should set handle position programmatically when divider orientation is vertical');
+  it('should set handle position programmatically when divider orientation is horizontal');
+  it('should reset to the default position programmatically');
 });
