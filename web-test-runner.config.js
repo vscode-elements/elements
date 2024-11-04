f991477658e82d44473099707611d6478308f65a
bendera <szoftverhiba@gmail.com>
2024-11-04 23:16:04 +0100

    Fix WTR console filter

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,4 +1,4 @@
-import { playwrightLauncher } from '@web/test-runner-playwright';
+import {playwrightLauncher} from '@web/test-runner-playwright';
 
 export default {
   files: ['./dist/**/*.test.js'],
@@ -6,7 +6,11 @@ export default {
   preserveSymlinks: true,
   browsers: [playwrightLauncher({product: 'chromium'})],
   filterBrowserLogs: ({args}) =>
-    !args[0]?.startsWith(
-      'Lit is in dev mode. Not recommended for production!'
-    ) && !args[0]?.includes('https://lit.dev/msg/change-in-update'),
+    !(
+      typeof args[0] === 'string' &&
+      (
+        args[0]?.startsWith('Lit is in dev mode. Not recommended for production!') ||
+        args[0]?.includes('https://lit.dev/msg/change-in-update')
+      )
+    ),
 };
