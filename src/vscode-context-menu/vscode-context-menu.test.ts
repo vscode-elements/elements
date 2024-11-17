febe141be8dc0be6ad8691ff2e95676ba4c7752f
bendera <szoftverhiba@gmail.com>
2024-11-17 21:02:39 +0100

    Fix `show` attribute synchronization

--- src/vscode-context-menu/vscode-context-menu.test.ts
+++ src/vscode-context-menu/vscode-context-menu.test.ts
@@ -1,9 +1,31 @@
+import { clickOnElement } from '../includes/test-helpers.js';
+import type {VscodeContextMenuItem} from '../main.js';
 import {VscodeContextMenu} from './index.js';
-import {expect} from '@open-wc/testing';
+import {expect, fixture, html} from '@open-wc/testing';
 
 describe('vscode-context-menu', () => {
   it('is defined', () => {
     const el = document.createElement('vscode-context-menu');
     expect(el).to.instanceOf(VscodeContextMenu);
+  });
+
+  it('should synchronize visibility state', async () => {
+    const el = await fixture<VscodeContextMenu>(
+      html`<vscode-context-menu show></vscode-context-menu>`
+    );
+    el.data = [
+      {label: 'Menu Item 1', value: 'menuitem1'},
+      {label: 'Menu Item 2', value: 'menuitem2'},
+    ];
+    await el.updateComplete;
+
+    const items = el.shadowRoot?.querySelectorAll<VscodeContextMenuItem>(
+      'vscode-context-menu-item'
+    )!;
+    await clickOnElement(items[0]);
+    await el.updateComplete;
+
+    expect(el.shadowRoot?.querySelector('vscode-context-menu-item')).to.be.null;
+    expect(el.hasAttribute('show')).to.be.false;
   });
 });
