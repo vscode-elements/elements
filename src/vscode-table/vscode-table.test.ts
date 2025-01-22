40498a1cc55e43f34b0a38127c9bf019629d68b2
Anthony Martin <38542602+anthony-c-martin@users.noreply.github.com>
2025-01-22 09:05:07 -0500

    Fix bug with element disconnection

--- src/vscode-table/vscode-table.test.ts
+++ src/vscode-table/vscode-table.test.ts
@@ -6,4 +6,11 @@ describe('vscode-table', () => {
     const el = document.createElement('vscode-table');
     expect(el).to.instanceOf(VscodeTable);
   });
+
+  it('should not throw when removed from the DOM', () => {
+    const el = document.createElement('vscode-table');
+    document.body.append(el);
+
+    expect(() => el.remove()).not.to.throw();
+  });
 });
