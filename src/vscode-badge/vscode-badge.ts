f0321233ddd371342cc1dc0ecc6d97aa360364f2
bendera <szoftverhiba@gmail.com>
2025-02-08 17:37:13 +0100

    Add fallback styles for Badge

--- src/vscode-badge/vscode-badge.ts
+++ src/vscode-badge/vscode-badge.ts
@@ -8,11 +8,12 @@ import styles from './vscode-badge.styles.js';
  *
  * @tag vscode-badge
  *
- * @cssprop --vscode-font-family
- * @cssprop --vscode-badge-background - default and counter variant background color
- * @cssprop --vscode-badge-foreground - default and counter variant foreground color
- * @cssprop --vscode-activityBarBadge-background - activity bar variant background color
- * @cssprop --vscode-activityBarBadge-foreground - activity bar variant foreground color
+ * @cssprop [--vscode-font-family=sans-serif] - A sans-serif font type depends on the host OS.
+ * @cssprop [--vscode-contrastBorder=transparent]
+ * @cssprop [--vscode-badge-background=#616161] - default and counter variant background color
+ * @cssprop [--vscode-badge-foreground=#f8f8f8] - default and counter variant foreground color
+ * @cssprop [--vscode-activityBarBadge-background=#0078d4] - activity bar variant background color
+ * @cssprop [--vscode-activityBarBadge-foreground=#ffffff] - activity bar variant foreground color
  */
 @customElement('vscode-badge')
 export class VscodeBadge extends VscElement {
