bd08164ff94af79e6a5e05d6cc5cbb2a664972ae
bendera <szoftverhiba@gmail.com>
2025-02-10 21:16:40 +0100

    Eslint fix

--- src/includes/vscode-select/vscode-select-base.ts
+++ src/includes/vscode-select/vscode-select-base.ts
@@ -581,7 +581,7 @@ export class VscodeSelectBase extends VscElement {
     });
 
     return html`
-      <div class="${classes}">
+      <div class=${classes}>
         ${this.position === 'above' ? this._renderDescription() : nothing}
         ${this._renderOptions()} ${this._renderDropdownControls()}
         ${this.position === 'below' ? this._renderDescription() : nothing}
@@ -591,7 +591,7 @@ export class VscodeSelectBase extends VscElement {
 
   override render(): TemplateResult {
     return html`
-      <slot class="main-slot" @slotchange="${this._onSlotChange}"></slot>
+      <slot class="main-slot" @slotchange=${this._onSlotChange}></slot>
       ${this.combobox ? this._renderComboboxFace() : this._renderSelectFace()}
       ${this.open ? this._renderDropdown() : nothing}
     `;
