f6fbf748959391c4a96cfe168f3147f32ae25341
bendera <szoftverhiba@gmail.com>
2025-03-08 16:32:11 +0100

    Add toggle mode to ToolbarButton

--- src/vscode-toolbar-button/vscode-toolbar-button.ts
+++ src/vscode-toolbar-button/vscode-toolbar-button.ts
@@ -1,14 +1,10 @@
 import {html, nothing, TemplateResult} from 'lit';
-import {
-  property,
-  queryAssignedElements,
-  queryAssignedNodes,
-  state,
-} from 'lit/decorators.js';
+import {property, queryAssignedNodes, state} from 'lit/decorators.js';
 import {customElement, VscElement} from '../includes/VscElement.js';
 import '../vscode-icon/vscode-icon.js';
 import styles from './vscode-toolbar-button.styles.js';
 import {classMap} from 'lit/directives/class-map.js';
+import {ifDefined} from 'lit/directives/if-defined.js';
 
 /**
  * Toolbar button
@@ -19,8 +15,17 @@ import {classMap} from 'lit/directives/class-map.js';
 export class VscodeToolbarButton extends VscElement {
   static override styles = styles;
 
-  @property()
+  @property({reflect: true})
   icon = '';
+
+  @property()
+  label: string | undefined = undefined;
+
+  @property({type: Boolean, reflect: true})
+  toggleable = false;
+
+  @property({type: Boolean, reflect: true})
+  checked = false;
 
   @state()
   _isSlotEmpty = true;
@@ -28,19 +33,38 @@ export class VscodeToolbarButton extends VscElement {
   @queryAssignedNodes()
   private _assignedNodes!: NodeList | null;
 
-  private _handleSlotChange = () => {
-    this._isSlotEmpty = !((this._assignedNodes?.length ?? 0) > 0)
-  };
+  private _handleSlotChange() {
+    this._isSlotEmpty = !((this._assignedNodes?.length ?? 0) > 0);
+  }
+
+  private _handleButtonClick() {
+    if (!this.toggleable) {
+      return;
+    }
+
+    this.checked = !this.checked;
+
+    this.dispatchEvent(new Event('change'));
+  }
 
   override render(): TemplateResult {
+    const checked = this.checked ? 'true' : 'false';
+
     return html`
-      <button type="button" class="button">
+      <button
+        type="button"
+        aria-label=${ifDefined(this.label)}
+        role=${ifDefined(this.toggleable ? 'switch' : undefined)}
+        aria-checked=${ifDefined(this.toggleable ? checked : undefined)}
+        class=${classMap({checked: this.toggleable && this.checked})}
+        @click=${this._handleButtonClick}
+      >
         ${this.icon
           ? html`<vscode-icon name=${this.icon}></vscode-icon>`
           : nothing}
         <slot
           @slotchange=${this._handleSlotChange}
-          class=${classMap({'empty': this._isSlotEmpty})}
+          class=${classMap({empty: this._isSlotEmpty, textOnly: !this.icon})}
         ></slot>
       </button>
     `;
