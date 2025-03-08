80bcb682c18e825348ca9c6070686f1fd8f8d6c3
bendera <szoftverhiba@gmail.com>
2025-03-08 17:17:54 +0100

    Update tests

--- src/vscode-toolbar-button/vscode-toolbar-button.test.ts
+++ src/vscode-toolbar-button/vscode-toolbar-button.test.ts
@@ -1,7 +1,7 @@
 import sinon from 'sinon';
 import {VscodeToolbarButton} from './index.js';
 import {expect, fixture, html} from '@open-wc/testing';
-import {sendKeys, sendMouse} from '@web/test-runner-commands';
+import {clickOnElement} from '../includes/test-helpers.js';
 
 describe('vscode-toolbar-button', () => {
   it('is defined', () => {
@@ -21,9 +21,28 @@ describe('vscode-toolbar-button', () => {
 
   it('sets accessible label to icon button', async () => {
     const el = await fixture(
-      html`<vscode-toolbar-button icon="account" label="Test label"></vscode-toolbar-button>`
+      html`<vscode-toolbar-button
+        icon="account"
+        label="Test label"
+      ></vscode-toolbar-button>`
     );
 
     await expect(el).to.be.accessible();
+  });
+
+  it('dispatch change event', async () => {
+    const el = await fixture(
+      html`<vscode-toolbar-button
+        icon="account"
+        label="Test label"
+        toggleable
+      ></vscode-toolbar-button>`
+    );
+    const spy = sinon.spy();
+    el.addEventListener('change', spy);
+
+    await clickOnElement(el);
+
+    expect(spy.called).to.be.true;
   });
 });
