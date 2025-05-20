89fc0c761ff8c144162393677862711f545e2cfe
Anthony Stewart <150152+a-stewart@users.noreply.github.com>
2025-05-20 02:15:01 +0200

    ONly take 1px off the wrapper since we don't have to match with padding

--- src/vscode-button/vscode-button.styles.ts
+++ src/vscode-button/vscode-button.styles.ts
@@ -105,7 +105,7 @@ const styles: CSSResultGroup = [
       display: flex;
       justify-content: center;
       position: relative;
-      width: calc(100% - 2px);
+      width: calc(100% - 1px);
       height: 100%;
       padding: 1px 13px;
     }
