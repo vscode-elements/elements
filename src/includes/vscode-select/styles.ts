60308596aa580052e6bc608d59ea7c9b3716324d
bendera <szoftverhiba@gmail.com>
2025-06-21 16:16:14 +0200

    Use scrollable component

--- src/includes/vscode-select/styles.ts
+++ src/includes/vscode-select/styles.ts
@@ -189,13 +189,18 @@ export default [
       border-color: var(--vscode-focusBorder, #0078d4);
     }
 
+    .scrollable {
+      display: block;
+      max-height: 222px;
+      margin: 0;
+      overflow: hidden;
+    }
+
     .options {
       box-sizing: border-box;
       cursor: pointer;
       list-style: none;
       margin: 0;
-      max-height: 222px;
-      overflow: auto;
       padding: 1px;
     }
 
