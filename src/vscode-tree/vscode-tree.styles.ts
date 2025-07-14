ad5adf30750ea3838c988264cb492e755c49d602
bendera <szoftverhiba@gmail.com>
2025-07-14 18:12:50 +0200

    Rename private css variables

--- src/vscode-tree/vscode-tree.styles.ts
+++ src/vscode-tree/vscode-tree.styles.ts
@@ -6,24 +6,24 @@ const styles: CSSResultGroup = [
   css`
     :host {
       --vsc-tree-item-arrow-display: none;
-      --selection-background: var(--vscode-list-inactiveSelectionBackground);
-      --selection-foreground: var(--vscode-foreground);
-      --selection-icon-foreground: var(--vscode-icon-foreground);
-      --default-indent-guide-display: none;
-      --highlighted-indent-guide-display: block;
+      --internal-selectionBackground: var(--vscode-list-inactiveSelectionBackground);
+      --internal-selectionForeground: var(--vscode-foreground);
+      --internal-selectionIconForeground: var(--vscode-icon-foreground);
+      --internal-defaultIndentGuideDisplay: none;
+      --internal-highlightedIndentGuideDisplay: block;
 
       display: block;
     }
 
     :host(:hover) {
-      --default-indent-guide-display: block;
-      --highlighted-indent-guide-display: block;
+      --internal-defaultIndentGuideDisplay: block;
+      --internal-highlightedIndentGuideDisplay: block;
     }
 
     :host(:focus-within) {
-      --selection-background: var(--vscode-list-activeSelectionBackground);
-      --selection-foreground: var(--vscode-list-activeSelectionForeground);
-      --selection-icon-foreground: var(--vscode-list-activeSelectionIconForeground);
+      --internal-selectionBackground: var(--vscode-list-activeSelectionBackground);
+      --internal-selectionForeground: var(--vscode-list-activeSelectionForeground);
+      --internal-selectionIconForeground: var(--vscode-list-activeSelectionIconForeground);
     }
 
     :host([arrows]) {
@@ -32,14 +32,14 @@ const styles: CSSResultGroup = [
 
     :host([indent-guides="none"]),
     :host([indent-guides="none"]:hover) {
-      --default-indent-guide-display: none;
-      --highlighted-indent-guide-display: none;
+      --internal-defaultIndentGuideDisplay: none;
+      --internal-highlightedIndentGuideDisplay: none;
     }
 
     :host([indent-guides="always"]),
     :host([indent-guides="always"]:hover) {
-      --default-indent-guide-display: block;
-      --highlighted-indent-guide-display: block;
+      --internal-defaultIndentGuideDisplay: block;
+      --internal-highlightedIndentGuideDisplay: block;
     }
   `,
 ];
