1380db9f91443f7079540be653ba8798996cc359
bendera <szoftverhiba@gmail.com>
2025-08-22 22:53:41 +0200

    Polish Breadcrumbs styles

--- src/vscode-breadcrumb-item/vscode-breadcrumb-item.styles.ts
+++ src/vscode-breadcrumb-item/vscode-breadcrumb-item.styles.ts
@@ -5,19 +5,17 @@ const styles: CSSResultGroup = [
   defaultStyles,
   css`
     :host {
-      display: inline-flex;
-      align-items: center;
-      gap: 6px;
-      color: var(--vscode-breadcrumb-foreground, inherit);
-      outline: none;
-      cursor: default;
+      display: inline-block;
     }
 
     :host(:focus) {
-      color: var(
-        --vscode-breadcrumb-focusForeground,
-        var(--vscode-breadcrumb-foreground, inherit)
-      );
+      outline: none;
+    }
+
+    :host(:focus) .root {
+      color: var(--vscode-breadcrumb-focusForeground, #e0e0e0);
+      outline: 1px solid var(--vscode-focusBorder, #0078d4);
+      text-decoration: underline;
     }
 
     :host(.selected) {
@@ -27,21 +25,45 @@ const styles: CSSResultGroup = [
       );
     }
 
+    .root {
+      display: flex;
+      align-items: center;
+      color: var(--vscode-breadcrumb-foreground, inherit);
+      outline: none;
+      cursor: pointer;
+    }
+
+    :host(:hover) .root {
+      color: var(--vscode-breadcrumb-focusForeground, #e0e0e0);
+    }
+
+    .icon {
+      height: 16px;
+      width: 16px;
+    }
+
+    .icon.has-icon {
+      margin-right: 6px;
+    }
+
     .separator {
       user-select: none;
       color: var(--vscode-breadcrumb-foreground, inherit);
+      height: 16px;
       opacity: 0.7;
+      width: 16px;
     }
 
     :host(:first-child) .separator {
       display: none;
     }
 
-    .icon::slotted(*) {
-      display: inline-flex;
-      align-items: center;
-      font-size: 12px;
-      line-height: 1;
+    :host(:first-child) .root {
+      margin-left: 16px;
+    }
+
+    :host(:last-child) .root {
+      margin-right: 8px;
     }
   `,
 ];
