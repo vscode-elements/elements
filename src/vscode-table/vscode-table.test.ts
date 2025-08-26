27cf74c5983359c60e59b7076d59e9ce4c4851d7
bendera <szoftverhiba@gmail.com>
2025-08-26 18:59:07 +0200

    Add test for table resize fix

--- src/vscode-table/vscode-table.test.ts
+++ src/vscode-table/vscode-table.test.ts
@@ -1,5 +1,6 @@
+import {$, dragElement} from '../includes/test-helpers.js';
 import {VscodeTable} from './index.js';
-import {expect} from '@open-wc/testing';
+import {expect, fixture, html} from '@open-wc/testing';
 
 describe('vscode-table', () => {
   it('is defined', () => {
@@ -12,5 +13,25 @@ describe('vscode-table', () => {
     document.body.append(el);
 
     expect(() => el.remove()).not.to.throw();
+  });
+
+  it('should not throw on resize when no rows are present', async () => {
+    const el = await fixture(html`
+      <vscode-table resizable style="width: 500px">
+        <vscode-table-header>
+          <vscode-table-header-cell>Col 1</vscode-table-header-cell>
+          <vscode-table-header-cell>Col 2</vscode-table-header-cell>
+        </vscode-table-header>
+        <vscode-table-body>
+          <vscode-table-row>no data</vscode-table-row>
+        </vscode-table-body>
+      </vscode-table>
+    `);
+
+    async function testDrag() {
+      await dragElement($(el.shadowRoot!, '.sash-clickable'), 20);
+    }
+
+    expect(await testDrag()).not.to.throw;
   });
 });
