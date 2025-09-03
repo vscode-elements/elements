613b60903e41c6d6e95c113f000aa95aa63300f2
bendera <szoftverhiba@gmail.com>
2025-09-04 00:26:25 +0200

    Add test for shadow DOM support

--- src/vscode-radio-group/vscode-radio-group.test.ts
+++ src/vscode-radio-group/vscode-radio-group.test.ts
@@ -1,8 +1,42 @@
 import {expect, fixture, html} from '@open-wc/testing';
-import {$} from '../includes/test-helpers.js';
+import {$, $$, clickOnElement} from '../includes/test-helpers.js';
 import '../vscode-radio/index.js';
 import {VscodeRadio} from '../vscode-radio/index.js';
 import {VscodeRadioGroup} from './index.js';
+
+class DemoElement extends HTMLElement {
+  static template = (() => {
+    const t = document.createElement('template');
+    t.innerHTML = `
+      <vscode-radio-group>
+        <vscode-radio value="1">Option 1</vscode-radio>
+        <vscode-radio value="2">Option 2</vscode-radio>
+        <vscode-radio value="3" default-checked>Option 3</vscode-radio>
+      </vscode-radio-group>
+    `;
+
+    return t;
+  })();
+
+  constructor() {
+    super();
+
+    const shadow = this.attachShadow({mode: 'open'});
+    shadow.appendChild(DemoElement.template.content.cloneNode(true));
+
+    const sheet = new CSSStyleSheet();
+    sheet.replaceSync(`
+      :host {
+        display: block;
+        padding: 16px;
+      }
+    `);
+
+    shadow.adoptedStyleSheets = [sheet];
+  }
+}
+
+customElements.define('demo-element', DemoElement);
 
 describe('vscode-radio-group', () => {
   it('is defined', () => {
@@ -47,5 +81,18 @@ describe('vscode-radio-group', () => {
 
     expect(icon2.classList.contains('checked')).to.be.false;
     expect(icon3.classList.contains('checked')).to.be.true;
+  });
+
+  it('works in shadow DOM', async () => {
+    const el = await fixture<DemoElement>(html`<demo-element></demo-element>`);
+
+    const radios = $$(el.shadowRoot!, 'vscode-radio');
+    const lastRadioDefaultChecked = radios[2].checked;
+
+    await clickOnElement(radios[0]);
+
+    expect(lastRadioDefaultChecked).to.be.true;
+    expect(radios[0].checked).to.be.true;
+    expect(radios[2].checked).to.be.false;
   });
 });
