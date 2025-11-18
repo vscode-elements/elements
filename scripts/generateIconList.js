6a0acc6f9692ca3de06adfbae542ce50eb0e75ce
dankeboy36 <dankeboy36@gmail.com>
2025-11-18 11:57:04 +0100

    feat: bump to `@vscode/codicons@0.0.42`
    Closes: vscode-elements/elements#557
    Signed-off-by: dankeboy36 <dankeboy36@gmail.com>

--- scripts/generateIconList.js
+++ scripts/generateIconList.js
@@ -1,19 +1,22 @@
 import fs from 'fs';
+import prettier from 'prettier';
 
 fs.readFile(
   './node_modules/@vscode/codicons/src/template/mapping.json',
   'utf-8',
-  (err, data) => {
+  async (err, data) => {
     if (err) {
       throw err;
     }
 
     const iconMap = JSON.parse(data);
     const keys = Object.keys(iconMap).sort();
+    const raw = keys
+      .map((k) => `<vscode-icon name="${k}" title="${k}"></vscode-icon>`)
+      .join('\n');
+    const formatted = await prettier.format(raw, {parser: 'html'});
 
-    keys.forEach((k) => {
-      // eslint-disable-next-line no-undef
-      console.log(`<vscode-icon name="${k}" title="${k}"></vscode-icon>`);
-    });
+    // eslint-disable-next-line no-undef
+    console.log(formatted);
   }
 );
